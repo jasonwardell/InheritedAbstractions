fe93ad70d249e61a2bde93d5b0cf579a58451aa4
klwortman <klwortman@192.168.1.3>
2015-09-03 08:57:29 -0500

    Changed Furniture to interface Couch now implements Furniture

--- src/inheritedabstractions/ChaseLounge.java
+++ src/inheritedabstractions/ChaseLounge.java
@@ -3,5 +3,10 @@ package inheritedabstractions;
 
 
 public class ChaseLounge extends couch{
-    
+couch c2 = new couch();
+
+   public void color() {
+       System.out.println("The color of the couch is " + c2.getColor()); 
+       
+}
 }
