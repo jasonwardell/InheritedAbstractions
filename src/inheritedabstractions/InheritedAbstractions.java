c679350766f2c228740a2a8f8fbd3c0a7ba4b132
klwortman <klwortman@192.168.1.3>
2015-09-03 08:52:32 -0500

    Added inputs and outputs in couch and ChaseLounge

--- src/inheritedabstractions/InheritedAbstractions.java
+++ src/inheritedabstractions/InheritedAbstractions.java
@@ -1,11 +1,20 @@
 package inheritedabstractions;
 
+import java.util.Scanner;
+
 
 public class InheritedAbstractions {
 
     
     public static void main(String[] args) {
+       
+        couch c1 = new couch();
+        ChaseLounge cl = new ChaseLounge();
+        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("What color is the couch?");
+        c1.setColor(keyboard.nextLine());
+        cl.color();
         
     }
-    
 }
