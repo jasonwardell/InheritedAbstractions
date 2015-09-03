75a32e2865192b454023b74154ff53839ef6748a
klwortman <klwortman@192.168.1.3>
2015-09-03 09:53:47 -0500

    working interface and inheritance

--- src/inheritedabstractions/InheritedAbstractions.java
+++ src/inheritedabstractions/InheritedAbstractions.java
@@ -2,19 +2,13 @@ package inheritedabstractions;
 
 import java.util.Scanner;
 
-
 public class InheritedAbstractions {
 
-    
     public static void main(String[] args) {
-       
-        couch c1 = new couch();
         ChaseLounge cl = new ChaseLounge();
-        
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("What color is the couch?");
-        c1.setColor(keyboard.nextLine());
-        cl.color();
-        
+        houseElf he = new houseElf();
+
+        System.out.println(cl.getColor());
+        he.getPowers();
     }
 }
