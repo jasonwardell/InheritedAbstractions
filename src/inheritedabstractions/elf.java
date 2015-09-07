824ff0b27c1d0a6b3a46b001ed45246785902951
klwortman <klwortman@192.168.1.3>
2015-09-06 20:08:01 -0500

    updated and final revised edition

--- src/inheritedabstractions/elf.java
+++ src/inheritedabstractions/elf.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package inheritedabstractions;
 
-/**
- *
- * @author klwortman
- */
-public class elf {
-    
+public class elf implements Creature {
+
+    @Override
+    public String powers() {
+        String powers = "Magic";
+        return powers;
+    }
 }
